f3929576240e3c94c78af3a7d242a054070f4093
Andres Freund <andres@anarazel.de>
2013-04-24 13:27:39 +0200

    bdr: mergme: Don't repetitively zero memory
    Alvaro Herrera

--- src/backend/replication/logical/replication_identifier.c
+++ src/backend/replication/logical/replication_identifier.c
@@ -204,11 +204,7 @@ ReplicationIdentifierShmemInit(void)
 
 	if (!found)
 	{
-		int i;
-		for (i = 0; i < max_logical_slots; i++)
-		{
-			MemSet(ReplicationStates, 0, ReplicationIdentifierShmemSize());
-		}
+		MemSet(ReplicationStates, 0, ReplicationIdentifierShmemSize());
 	}
 }
 
