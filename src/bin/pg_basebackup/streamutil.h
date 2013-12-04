9240d3a2efd0f74d5670f0a200ba458d69b8b49c
Andres Freund <andres@anarazel.de>
2013-12-04 16:37:38 +0100

    wal_decoding: pg_recvlogical: Introduce pg_receivexlog equivalent for logical changes

--- src/bin/pg_basebackup/streamutil.h
+++ src/bin/pg_basebackup/streamutil.h
@@ -5,6 +5,7 @@ extern char *connection_string;
 extern char *dbhost;
 extern char *dbuser;
 extern char *dbport;
+extern char *dbname;
 extern int	dbgetpassword;
 
 /* Connection kept global so we can disconnect easily */
@@ -17,3 +18,12 @@ extern PGconn *conn;
 	}
 
 extern PGconn *GetConnection(void);
+
+extern int64 feGetCurrentTimestamp(void);
+extern void feTimestampDifference(int64 start_time, int64 stop_time,
+									 long *secs, int *microsecs);
+
+extern bool feTimestampDifferenceExceeds(int64 start_time, int64 stop_time,
+											int msec);
+extern void fe_sendint64(int64 i, char *buf);
+extern int64 fe_recvint64(char *buf);
