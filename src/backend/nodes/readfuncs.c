2a24b68413b90779bfb5bff97b505c5aadc188a7
Craig Ringer <craig@2ndquadrant.com>
2014-01-20 13:28:12 +0800

    RLS: Add rowsec_relid to ResultRelInfo
    This hunk may still be needed in the new approach to filter out rows that've
    changed to no longer be visible due to user action r action of triggers;
    we'd need to filter the result relation.

--- src/backend/nodes/readfuncs.c
+++ src/backend/nodes/readfuncs.c
@@ -1218,6 +1218,7 @@ _readRangeTblEntry(void)
 		case RTE_SUBQUERY:
 			READ_NODE_FIELD(subquery);
 			READ_BOOL_FIELD(security_barrier);
+			READ_OID_FIELD(rowsec_relid);
 			break;
 		case RTE_JOIN:
 			READ_ENUM_FIELD(jointype, JoinType);
