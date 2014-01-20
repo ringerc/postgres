6785c6f797196e9312664877739ca38976232e77
Craig Ringer <craig@2ndquadrant.com>
2014-01-20 13:07:50 +0800

    RLS: add pg_rowsecurity catalog

--- src/include/catalog/indexing.h
+++ src/include/catalog/indexing.h
@@ -313,6 +313,11 @@ DECLARE_UNIQUE_INDEX(pg_extension_name_index, 3081, on pg_extension using btree(
 DECLARE_UNIQUE_INDEX(pg_range_rngtypid_index, 3542, on pg_range using btree(rngtypid oid_ops));
 #define RangeTypidIndexId					3542
 
+DECLARE_UNIQUE_INDEX(pg_rowsecurity_oid_index, 5001, on pg_rowsecurity using btree(oid oid_ops));
+#define RowSecurityOidIndexId				5001
+DECLARE_UNIQUE_INDEX(pg_rowsecurity_relid_index, 5002, on pg_rowsecurity using btree(rsecrelid oid_ops, rseccmd char_ops));
+#define RowSecurityRelidIndexId				5002
+
 /* last step of initialization script: build the indexes declared above */
 BUILD_INDICES
 
